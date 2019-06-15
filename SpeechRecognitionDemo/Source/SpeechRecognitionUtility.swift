40a7e9d53c006bf9f65906d8ccb1115c662edbe0
Jayesh Kawli <jkawli@wayfair.com>
2019-06-15 19:22:56 -0400

    Created base project with no feature and just UI

--- SpeechRecognitionDemo/Source/SpeechRecognitionUtility.swift
+++ SpeechRecognitionDemo/Source/SpeechRecognitionUtility.swift
@@ -10,146 +10,33 @@ import Foundation
 import Speech
 
 enum SpeechRecognitionOperationError: Error {
-    case denied
-    case notDetermined
-    case restricted
-    case audioSessionUnavailable
-    case invalidRecognitionRequest
-    case audioEngineUnavailable
+
 }
 
 enum SpeechRecognitionOperationState {
-    case denied
-    case authorized
-    case audioEngineStart
-    case audioEngineStop
-    case recognitionTaskCancelled
-    case speechRecognized(String)
-    case speechNotRecognized
-    // Called when speech recognition is done and we're ready to send those strings to server for translations
-    case speechRecognitionStopped(String)
+
 }
 
 class SpeechRecognitionUtility: NSObject {
 
-    private let speechRecognizer: SFSpeechRecognizer?
+    private var speechRecognizer: SFSpeechRecognizer!
     private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
     private var recognitionTask: SFSpeechRecognitionTask?
     private var audioEngine: AVAudioEngine?
-    private let recognitionStateUpdateBlock: (SpeechRecognitionOperationState, Bool) -> Void
-    private var speechRecognitionPermissionState: SFSpeechRecognizerAuthorizationStatus
-    private let speechRecognitionAuthorizedBlock: () -> Void
-    private var recognizedText: String
+    private var recognitionStateUpdateBlock: ((SpeechRecognitionOperationState, Bool) -> Void)?
+    private var speechRecognitionPermissionState: SFSpeechRecognizerAuthorizationStatus = .notDetermined
+    private var speechRecognitionAuthorizedBlock: (() -> Void)?
+    private var recognizedText: String = ""
 
     init(speechRecognitionAuthorizedBlock : @escaping () -> Void, stateUpdateBlock: @escaping (SpeechRecognitionOperationState, Bool) -> Void) {
-        speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en_US"))
-        recognitionStateUpdateBlock = stateUpdateBlock
-        // let supportedLocales = SFSpeechRecognizer.supportedLocales()
-        speechRecognitionPermissionState = .notDetermined
-        self.speechRecognitionAuthorizedBlock = speechRecognitionAuthorizedBlock
-        self.recognizedText = ""
-
-        super.init()
-
-        SFSpeechRecognizer.requestAuthorization { (status) in
-            self.speechRecognitionPermissionState = status
-            if status == .authorized {
-                self.audioEngine = AVAudioEngine()
-                // Need to return it on Main queue since this block is returned on serial queue. Assuming user wants to do UI actions once request is authorized.
-                OperationQueue.main.addOperation {
-                    speechRecognitionAuthorizedBlock()
-                }
-            } else {
-                // show denial message on UI
-                self.recognitionStateUpdateBlock(.denied, true)
-            }
-        }
     }
 
     func startSpeechRecognition() throws {
-        switch self.speechRecognitionPermissionState {
-        case .denied:
-            throw SpeechRecognitionOperationError.denied
-        case .notDetermined:
-            throw SpeechRecognitionOperationError.notDetermined
-        case .restricted:
-            throw SpeechRecognitionOperationError.restricted
-        case .authorized:
-            print("User authorized app to access speech recognition")
-        }
 
-        if audioEngine?.isRunning ?? false {
-            audioEngine?.stop()
-            recognitionRequest?.endAudio()
-        } else {
-            try self.runSpeechRecognition()
-        }
     }
 
     func runSpeechRecognition() throws  {
 
-        if recognitionTask != nil {
-            recognitionTask?.cancel()
-            self.updateSpeechRecognitionState(with: .recognitionTaskCancelled)
-            recognitionTask = nil
-        }
-
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(AVAudioSessionCategoryRecord)
-            try audioSession.setMode(AVAudioSessionModeMeasurement)
-            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
-        } catch let error {
-            print("Error Occurred: \(error.localizedDescription)")
-            throw SpeechRecognitionOperationError.audioSessionUnavailable
-        }
-
-        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
-
-        guard let audioEngine = audioEngine else {
-            throw SpeechRecognitionOperationError.audioEngineUnavailable
-        }
-
-        let inputNode = audioEngine.inputNode
-
-        guard let recognitionRequest = recognitionRequest else {
-            throw SpeechRecognitionOperationError.invalidRecognitionRequest
-        }
-
-        recognitionRequest.shouldReportPartialResults = false
-
-        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { [weak self] (result, error) in
-            guard let strongSelf = self else { return }
-            // Hypotheses for possible transcriptions, sorted in decending order of confidence (more likely first)
-            // result.transcriptions
-            if let result = result {
-                strongSelf.updateSpeechRecognitionState(with: .speechRecognized(result.bestTranscription.formattedString))
-            } else {
-                strongSelf.updateSpeechRecognitionState(with: .speechNotRecognized)
-            }
-        })
-
-        let recordingFormat = inputNode.outputFormat(forBus: 0)
-
-        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, time) in
-            self.recognitionRequest?.append(buffer)
-        }
-
-        audioEngine.prepare()
-
-        if let _ = try? audioEngine.start() {
-            self.updateSpeechRecognitionState(with: .audioEngineStart)
-        } else {
-            throw SpeechRecognitionOperationError.audioEngineUnavailable
-        }
-    }
-
-    func toggleSpeechRecognitionActivity() throws {
-        if self.isSpeechRecognitionOn() == true {
-            self.stopAudioRecognition()
-        } else {
-            try self.runSpeechRecognition()
-        }
     }
 
     func speechFinished() {
@@ -158,32 +45,11 @@ class SpeechRecognitionUtility: NSObject {
 
     // A method to stop audio engine thereby stopping device to input user audio and process it. It will remove the input source from specified Bus.
     private func stopAudioRecognition() {
-        guard let audioEngine = audioEngine else { return }
-        if audioEngine.isRunning {
-            audioEngine.stop()
-            self.recognitionRequest?.endAudio()
-            self.updateSpeechRecognitionState(with: .audioEngineStop)
-            self.updateSpeechRecognitionState(with: .speechRecognitionStopped(recognizedText))
-            audioEngine.inputNode.removeTap(onBus: 0)
-        }
 
-        self.recognitionRequest = nil
-
-        if self.recognitionTask != nil {
-            self.recognitionTask?.cancel()
-            self.updateSpeechRecognitionState(with: .recognitionTaskCancelled)
-            self.recognitionTask = nil
-        }
-    }
-
-
-    private func updateSpeechRecognitionState(with state: SpeechRecognitionOperationState, finalOutput: Bool = false) {
-        OperationQueue.main.addOperation {
-            self.recognitionStateUpdateBlock(state, finalOutput)
-        }
     }
 
     private func isSpeechRecognitionOn() -> Bool {
-        return self.audioEngine?.isRunning ?? false
+        //return self.audioEngine?.isRunning ?? false
+        return false
     }
 }
--- SpeechRecognitionDemo/ViewController.swift
+++ SpeechRecognitionDemo/ViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 import Speech
 
-let maximumAllowedTimeDuration = 30
-
 class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
     let reachability = Reachability()!
@@ -37,24 +35,15 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
 
     @IBOutlet weak var requestTranslationsButton: UIButton!
 
-    // A utility to easily use the speech recognition facility.
-    var speechRecognizerUtility: SpeechRecognitionUtility?
-
-    private var timer: Timer?
-    private var totalTime: Int = 0
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        reachability.whenReachable = { [weak self] reachable in
-            self?.toggleSpeechButtonAccessState(enabled: true)
-            self?.speechButton.setTitle("Begin New Translation", for: .normal)
+        reachability.whenReachable = { reachable in
+
         }
 
-        reachability.whenUnreachable = {[weak self] _ in
-            self?.speechButton.setTitle("No network connectivity", for: .normal)
-            self?.toggleSpeechButtonAccessState(enabled: false)
-            self?.speechFinishedButton.isHidden = true
+        reachability.whenUnreachable = { _ in
+
         }
 
         do {
@@ -64,175 +53,29 @@ class ViewController: UIViewController, SFSpeechRecognizerDelegate {
         }
 
         self.title = "Translations App"
-        speechButton.setTitle("Begin Translation...", for: .normal)
-        speechTextLabel.text = "Press Begin Translation button to start translation"
-        translatedTextLabel.text = ""
-        timeLimiterIndicatorLabel.backgroundColor = .green
-        timeLimiterIndicatorLabel.textAlignment = .center
-        timeLimiterIndicatorLabel.font = UIFont.systemFont(ofSize: 12)
-        timeLimiterIndicatorLabel.text = "0"
-        statusLabel.text = ""
-        speechFinishedButton.addTarget(self, action: #selector(speechFinished), for: .touchUpInside)
-        requestTranslationsButton.addTarget(self, action: #selector(requestTranslations), for: .touchUpInside)
     }
 
-    @objc func speechFinished() {
-        speechRecognizerUtility?.speechFinished()
-        speechFinishedButton.isHidden = true
-        speechButton.setTitle(nil, for: .normal)
+    @IBAction func speechFinished() {
+
     }
 
-    @objc func requestTranslations() {
-        toggleSpeechRecognitionState()
-        stopTimeCounter()
-        requestTranslationsFromServer()
+    @IBAction func requestTranslations() {
+
     }
 
     @IBAction func saySomethingButtonPressed(_ sender: Any) {
-        if speechRecognizerUtility == nil {
-            // Initialize the speech recognition utility here
-            speechRecognizerUtility = SpeechRecognitionUtility(speechRecognitionAuthorizedBlock: { [weak self] in
-                self?.toggleSpeechRecognitionState()
-            }, stateUpdateBlock: { [weak self] (currentSpeechRecognitionState, finalOutput) in
-                // A block to update the status of speech recognition. This block will get called every time Speech framework recognizes the speech input
-                self?.stateChangedWithNew(state: currentSpeechRecognitionState)
-                // We won't perform translation until final input is ready. We will usually wait for users to finish speaking their input until translation request is sent
-                if finalOutput {
-                    self?.toggleSpeechRecognitionState()
-                }
-            })
-        } else {
-            // We will call this method to toggle the state on/off of speech recognition operation.
-            self.toggleSpeechRecognitionState()
-        }
+
     }
 
     func requestTranslationsFromServer() {
         // Trigger the request to get translations as soon as user has done providing full speech input. Don't trigger until query length is at least one.
-        self.speechButton.setTitle("Getting translations.....", for: .normal)
-        if let query = self.speechTextLabel.text, query.count > 0 {
-            self.statusLabel.text = "Please wait while we get translations from server"
-            // Disable the toggle speech button while we're getting translations from server.
-            toggleSpeechButtonAccessState(enabled: false)
-            NetworkRequest.sendRequestWith(query: query, completion: { (translation) in
-                OperationQueue.main.addOperation {
-                    // Explicitly execute the code on main thread since the request we get back need not be on the main thread.
-                    self.translatedTextLabel.text = translation
-                    self.resetState()
-                }
-            })
-        } else {
-            resetState()
-        }
     }
 
     func resetState() {
-        self.statusLabel.text = ""
-        self.speechButton.setTitle("Begin New Translation", for: .normal)
-        // Re-enable the toggle speech button once translations are ready.
-        self.requestTranslationsButton.setTitle(nil, for: .normal)
-        self.toggleSpeechButtonAccessState(enabled: true)
+
     }
 
-    // A method to toggle the userInteractionState of toggle speech state button
     func toggleSpeechButtonAccessState(enabled: Bool) {
-        self.speechButton.isUserInteractionEnabled = enabled
-        if enabled {
-            self.speechButton.alpha = 1.0
-            speechFinishedButton.isHidden = true
-        } else {
-            self.speechButton.alpha = 0.6
-        }
-    }
 
-    // A method to toggle the speech recognition state between on/off
-    private func toggleSpeechRecognitionState() {
-        do {
-            try self.speechRecognizerUtility?.toggleSpeechRecognitionActivity()
-        } catch SpeechRecognitionOperationError.denied {
-            print("Speech Recognition access denied")
-        } catch SpeechRecognitionOperationError.notDetermined {
-            print("Unrecognized Error occurred")
-        } catch SpeechRecognitionOperationError.restricted {
-            print("Speech recognition access restricted")
-        } catch SpeechRecognitionOperationError.audioSessionUnavailable {
-            print("Audio session unavailable")
-        } catch SpeechRecognitionOperationError.invalidRecognitionRequest {
-            print("Recognition request is null. Expected non-null value")
-        } catch SpeechRecognitionOperationError.audioEngineUnavailable {
-            print("Audio engine is unavailable. Cannot perform speech recognition")
-        } catch {
-            print("Unknown error occurred")
-        }
-    }
-
-    private func stateChangedWithNew(state: SpeechRecognitionOperationState) {
-        switch state {
-            case .denied:
-                print("Access to Speech Recognizer denied")
-            case .authorized:
-                print("State: Speech recognition authorized")
-            case .audioEngineStart:
-                self.speechTextLabel.text = "Please say something to translate"
-                self.speechButton.setTitle("Listening....", for: .normal)
-                self.speechFinishedButton.isHidden = false
-                toggleSpeechButtonAccessState(enabled: false)
-                self.startTimeCounterAndUpdateUI()
-                speechButton.setTitleColor(.red, for: .normal)
-                translatedTextLabel.text = ""
-                self.speechFinishedButton.isHidden = false
-                self.recordingIndicator.isHidden = false
-                print("State: Audio Engine Started")
-            case .audioEngineStop:
-                print("State: Audio Engine Stopped")
-            self.recordingIndicator.isHidden = true
-            case .recognitionTaskCancelled:
-                print("State: Recognition Task Cancelled")
-            case .speechRecognized(let recognizedString):
-                self.speechTextLabel.text = recognizedString
-                self.requestTranslationsButton.setTitle("Translate", for: .normal)
-                speechButton.setTitleColor(.green, for: .normal)
-                print("State: Recognized String \(recognizedString)")
-            case .speechNotRecognized:
-                print("State: Speech Not Recognized")
-            // Called when speech recognition is done, audio engine is stopped and strings are finally sent for translation on the server.
-            case .speechRecognitionStopped(let finalRecognizedString):
-                self.stopTimeCounter()
-                speechButton.setTitleColor(.green, for: .normal)
-                print("State: Speech Recognition Stopped with final string \(finalRecognizedString)")
-        }
-    }
-
-    private func startTimeCounterAndUpdateUI() {
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [weak self] (timer) in
-            guard let weakSelf = self else { return }
-
-            guard weakSelf.totalTime < maximumAllowedTimeDuration else {
-                weakSelf.speechTextLabel.text = "Maximum time out reached. Please start over"
-                weakSelf.toggleSpeechRecognitionState()
-                weakSelf.resetState()
-                return
-            }
-
-            weakSelf.totalTime = weakSelf.totalTime + 1
-
-            if weakSelf.totalTime >= 2 * (maximumAllowedTimeDuration / 3) {
-                weakSelf.timeLimiterIndicatorLabel.backgroundColor = .red
-            } else if weakSelf.totalTime >= maximumAllowedTimeDuration / 3 {
-                weakSelf.timeLimiterIndicatorLabel.backgroundColor = .orange
-            } else {
-                weakSelf.timeLimiterIndicatorLabel.backgroundColor = .green
-            }
-            weakSelf.timeLimiterIndicatorLabel.text = "\(weakSelf.totalTime)"
-        })
-    }
-
-    private func stopTimeCounter() {
-        self.timer?.invalidate()
-        self.timer = nil
-        self.totalTime = 0
-        self.timeLimiterIndicatorLabel.backgroundColor = .green
-        self.timeLimiterIndicatorLabel.text = "0"
     }
 }
-
